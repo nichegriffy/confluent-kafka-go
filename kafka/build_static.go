b2d442010203910c23f122e866c92be6c9ff8191
Joel Griffith <jgriffith@niche.com>
2018-02-22 21:21:53 -0500

    Use linux package file and only link what is used

--- kafka/build_static.go
+++ kafka/build_static.go
@@ -4,8 +4,7 @@
 package kafka
 
 // #cgo linux CFLAGS: -I${SRCDIR}/clib/include
-// linux LDFLAGS: -L${SRCDIR}/clib/lib -Wl,-Bstatic -lrdkafka -Wl,-Bdynamic -lpthread -lz -lcrypto -lz -ldl
-// #cgo linux LDFLAGS: -L${SRCDIR}/clib/lib -Wl,-Bstatic -lrdkafka
+// #cgo linux LDFLAGS: -L${SRCDIR}/clib/lib -Wl,-Bstatic -lrdkafka -Wl,-Bdynamic -lpthread -ldl
 // #cgo !linux pkg-config: --static rdkafka
 // #cgo !linux LDFLAGS: -Wl,-Bstatic -lrdkafka -Wl,-Bdynamic
 import "C"
