15ddac5d2293b1f1ec728c68a349b5722f4cdfef
Joel Griffith <jgriffith@niche.com>
2018-02-22 20:42:49 -0500

    Vendor librdkafka under clib directory and update build flags to use it

--- kafka/build_dynamic.go
+++ kafka/build_dynamic.go
@@ -3,6 +3,8 @@
 
 package kafka
 
-// #cgo pkg-config: rdkafka
-// #cgo LDFLAGS: -lrdkafka
+// #cgo darwin linux CFLAGS: -I${SRCDIR}/clib/include
+// #cgo darwin linux LDFLAGS: -L${SRCDIR}/clib/lib -lrdkafka
+// #cgo !darwin,!linux pkg-config: rdkafka
+// #cgo !darwin,!linux LDFLAGS: -lrdkafka
 import "C"
--- kafka/build_static.go
+++ kafka/build_static.go
@@ -3,6 +3,9 @@
 
 package kafka
 
-// #cgo pkg-config: --static rdkafka
-// #cgo LDFLAGS: -Wl,-Bstatic -lrdkafka -Wl,-Bdynamic
+// #cgo linux CFLAGS: -I${SRCDIR}/clib/include
+// linux LDFLAGS: -L${SRCDIR}/clib/lib -Wl,-Bstatic -lrdkafka -Wl,-Bdynamic -lpthread -lz -lcrypto -lz -ldl
+// #cgo linux LDFLAGS: -L${SRCDIR}/clib/lib -Wl,-Bstatic -lrdkafka
+// #cgo !linux pkg-config: --static rdkafka
+// #cgo !linux LDFLAGS: -Wl,-Bstatic -lrdkafka -Wl,-Bdynamic
 import "C"
